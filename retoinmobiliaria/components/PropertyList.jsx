95010e3ccc0e15524dcd0ae76151df971732cfa3
Tole15 <rodrigermoon@gmail.com>
2023-06-12 03:45:29 -0600

    Created PropertyList component for displaying a list of properties
    Implemented the PropertyList component to display a list of properties. This component will be responsible for rendering multiple PropertyCard components in a FlatList.

---
+++ retoinmobiliaria/components/PropertyList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import PropertyCard from './Card.jsx';
+import properties from '../data/properties.json';
+
+const PropertyList = () => {
+  const renderProperty = ({ item }) => (
+    <PropertyCard
+      name={item.Name}
+      location={item.Details[0].Location}
+      bedrooms={item.Details[0].Bedrooms}
+      bathrooms={item.Details[0].Bathrooms}
+      space={item.Details[0].Space}
+      price={item.Details[0].Price}
+      image={item.Details[0].image}
+    />
+  );
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={properties.Properties}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderProperty}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default PropertyList;
