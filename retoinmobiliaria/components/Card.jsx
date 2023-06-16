f6c4ca2b471e941040cf5d54c3427b71a2b81bc2
Tole15 <rodrigermoon@gmail.com>
2023-06-16 13:51:45 -0600

    Fixed prop type validation and ESLint errors
    In this commit, I addressed the prop type validation issues in the Card.jsx component and resolved the ESLint errors. I removed the extra semicolons that were causing the errors and ensured proper syntax throughout the file. By fixing the prop type validations and resolving the ESLint errors, the code now adheres to best practices and maintains a clean and error-free codebase.

--- retoinmobiliaria/components/Card.jsx
+++ retoinmobiliaria/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
+import PropTypes from 'prop-types'
 import LikeButton from './LikeButton'
 import {
   Ionicons,
@@ -33,61 +34,68 @@ const PropertyCard = ({
     <View style={styles.cardContainer}>
       <View style={styles.rowContainer}>
         <View>
-        <Image style={styles.image} source={{ uri: image }}/>
+          <Image style={styles.image} source={{ uri: image }} />
 
-        <View style={styles.starContainer}>
-          <FontAwesome
-          name="star"
-          size={13}
-          color={colors.StarIcon}
-          />
-          <Text style={styles.StarText}>{rate}</Text>
+          <View style={styles.starContainer}>
+            <FontAwesome name="star" size={13} color={colors.StarIcon} />
+            <Text style={styles.StarText}>{rate}</Text>
           </View>
-          </View>
-          <View style={styles.infoContainer}>
-            <Text style={styles.title}>{name}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{name}</Text>
 
-            <View style={styles.addressContainer}>
-              <SimpleLineIcons
-                name="location-pin"
+          <View style={styles.addressContainer}>
+            <SimpleLineIcons
+              name="location-pin"
+              size={22}
+              color={colors.icons}
+            />
+            <Text style={styles.ubi}>{location}</Text>
+          </View>
+
+          <View style={styles.rowContainer}>
+            <View style={styles.iconTextContainer}>
+              <Ionicons name="bed-outline" size={22} color={colors.icons} />
+              <Text style={styles.infoText}>{bedrooms}</Text>
+            </View>
+
+            <View style={styles.iconTextContainer}>
+              <MaterialCommunityIcons
+                name="shower"
                 size={22}
                 color={colors.icons}
+                style={styles.mirrorIcon}
               />
-              <Text style={styles.ubi}>{location}</Text>
+              <Text style={styles.infoText}>{bathrooms}</Text>
             </View>
 
-            <View style={styles.rowContainer}>
-              <View style={styles.iconTextContainer}>
-                <Ionicons name="bed-outline" size={22} color={colors.icons} />
-                <Text style={styles.infoText}>{bedrooms}</Text>
-              </View>
-
-              <View style={styles.iconTextContainer}>
-                <MaterialCommunityIcons
-                  name="shower"
-                  size={22}
-                  color={colors.icons}
-                  style={styles.mirrorIcon}
-                />
-                <Text style={styles.infoText}>{bathrooms}</Text>
-              </View>
-
-              <View style={styles.iconTextContainer}>
-                <MaterialCommunityIcons
-                  name="floor-plan"
-                  size={22}
-                  color={colors.icons}
-                  style={styles.mirrorIconDos}
-                />
-                <Text style={styles.infoText}>{space}</Text>
-              </View>
+            <View style={styles.iconTextContainer}>
+              <MaterialCommunityIcons
+                name="floor-plan"
+                size={22}
+                color={colors.icons}
+                style={styles.mirrorIconDos}
+              />
+              <Text style={styles.infoText}>{space}</Text>
             </View>
-            <Text style={styles.price}>{price}</Text>
           </View>
-          <LikeButton/>
+          <Text style={styles.price}>{price}</Text>
+        </View>
+        <LikeButton />
       </View>
     </View>
   )
+}
+
+PropertyCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  bedrooms: PropTypes.number.isRequired,
+  bathrooms: PropTypes.number.isRequired,
+  space: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rate: PropTypes.string.isRequired
 }
 
 export default PropertyCard
